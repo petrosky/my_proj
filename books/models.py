89bf9a13a9ce35e8981d64feb9a68657bdb1adeb
petrosky <1aIIbXXufd77ceKCyktU>
2019-03-21 10:29:47 +1100

    Added Genres

--- books/models.py
+++ books/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=75)
+
+
 class Book(models.Model):
     name = models.CharField(max_length=200)
     pages = models.IntegerField()
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.name
@@ -12,10 +25,3 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book_edit', kwargs={'pk': self.pk})
 
-
-class Genre(models.Model):
-    name = models.CharField(max_length=50)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
